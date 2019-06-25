fecf9fd0b3a24cad830d4ee6161556264fb395c1
Fayçal BORDJAH <fbordjah@linagora.com>
2019-06-25 10:02:28 +0200

    Create find method on Conference Api

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,7 +4,29 @@
 namespace App\Controller;
 
 
-class ConferenceController
+use App\Entity\Conference;
+use App\Repository\ConferenceRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ConferenceController
+ * @package App\Controller
+ * @Route(path="/",name="conference")
+ */
+class ConferenceController extends AbstractController
 {
 
+    /**
+     * @param int $id
+     * @return Response
+     * @Route(path="/show/{id}")
+     */
+public function find(int $id):Response{
+    /**@var ConferenceRepository $repository*/
+   $repository = $this->getDoctrine()->getRepository(Conference::class);
+   return new Response("TesT");
+
+}
 }

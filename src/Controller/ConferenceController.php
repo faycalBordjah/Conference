56fc9dc2474a71f872220e78d51d63eaf6d99374
Fayçal BORDJAH <fbordjah@linagora.com>
2019-06-25 11:43:58 +0200

    Add first methods to create Conference Api

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,8 +3,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ConferenceController
+/**
+ * Class ConferenceController
+ * @package App\Controller
+ * @Route(path="/",name="conference")
+ */
+class ConferenceController extends AbstractController
 {
+    /**
+     * @Route(path="/get/{id}")
+     * @param int $id
+     */
+    public function find(int $id): Response
+    {
+        /** @var ConferenceRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(Conference::class);
+        //$article = $repository->find($id);
+        return new Response("Test");
+    }
 
+    /**
+     * @param PaginatorInterface $paginator
+     * @return Response
+     */
+    public function index(Request $request,PaginatorInterface $paginator) :Response{
+        /** @var ConferenceRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(Conference::class);
+        $conference = $repository->createQueryBuilder('a')
+            ->orderBy('a.creationDate', 'DESC')
+            ->getQuery();
+        $conference = $paginator->paginate($conference, $request->query->getInt('page', 1), 10);
+
+    }
 }

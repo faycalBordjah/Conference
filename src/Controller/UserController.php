eb6a3617a43f9944ab23338bf12814146eb42d4b
Fayçal BORDJAH <fbordjah@linagora.com>
2019-06-26 14:54:47 +0200

    Add user api

---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class UserController
+ * @package App\Controller
+ * @Route(path="/users",name="users_api")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route(path="/findAll")
+     * @return Response
+     */
+    public function findAll(){
+        /**@var \App\Repository\UserRepository $repository*/
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $users = $repository->findAll();
+        return new Response("users");
+    }
+    /**
+     * @Route(path="/find/{id}")
+     * @return Response
+     */
+    public function find(int $id) :Response{
+        /**@var \App\Repository\UserRepository $repository*/
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $users = $repository->find($id);
+        var_dump($users);
+        return new Response("user");
+    }
+}

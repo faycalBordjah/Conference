231c631f31e46d7508d55e64a6199e4eefc5790f
Fayçal BORDJAH <fbordjah@linagora.com>
2019-06-29 13:10:24 +0200

    put all methods related to magae users in conference controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * Class ConferenceController
@@ -168,5 +169,54 @@ class ConferenceController extends AbstractController
     public function topTen(Request $request): Response
     {
         return $this->render('');
+    }
+    /**
+     * @Route(path="/users/findAll",name="users")
+     * @return Response
+     */
+    public function findAllUsers()
+    {
+        /**@var \App\Repository\UserRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $users = $repository->findAll();
+        return $this->render('user/users.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+    /**
+     * @Route(path="/users/find/{id}",name="user")
+     * @return Response
+     */
+    public function findUser(User $user): Response
+    {
+        /**@var \App\Repository\UserRepository $repository */
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->find($user->getId());
+        return $this->render('user/user.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @param User $user
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route(path="/users/delete/{id}",name="user_delete")
+     */
+    public function deleteUser(Request $request, User $user): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entity = $em->getRepository(User::class)->find($user->getId());
+        $roles = $user->getRoles();
+        if (in_array('ROLE_ADMIN', $roles)) {
+            throw new AccessDeniedException('Can not delete the administrator');
+        }
+
+        if (!$entity) {
+            throw $this->createNotFoundException('user not found');
+        }
+        $em->remove($user);
+        $em->flush();
+        return $this->redirectToRoute('users');
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -17,53 +17,5 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
  */
 class UserController extends AbstractController
 {
-    /**
-     * @Route(path="/findAll",name="users")
-     * @return Response
-     */
-    public function findAll()
-    {
-        /**@var \App\Repository\UserRepository $repository */
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        $users = $repository->findAll();
-        return $this->render('user/users.html.twig', [
-            'users' => $users
-        ]);
-    }
 
-    /**
-     * @Route(path="/find/{id}",name="user")
-     * @return Response
-     */
-    public function find(User $user): Response
-    {
-        /**@var \App\Repository\UserRepository $repository */
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        $user = $repository->find($user->getId());
-        return $this->render('user/user.html.twig', [
-            'user' => $user
-        ]);
-    }
-
-    /**
-     * @param User $user
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     * @Route(path="/delete/{id}",name="user_delete")
-     */
-    public function delete(Request $request, User $user): Response
-    {
-        $em = $this->getDoctrine()->getManager();
-        $entity = $em->getRepository(User::class)->find($user->getId());
-        $roles = $user->getRoles();
-        if (in_array('ROLE_ADMIN', $roles)) {
-            throw new AccessDeniedException('Can not delete the administrator');
-        }
-
-        if (!$entity) {
-            throw $this->createNotFoundException('user not found');
-        }
-        $em->remove($user);
-        $em->flush();
-        return $this->redirectToRoute('users');
-    }
 }

1aa578979512d513eae88049f5ae8a81915e6667
Fayçal BORDJAH <fbordjah@linagora.com>
2019-06-30 11:19:59 +0200

    create a view for rate

--- src/Controller/RateController.php
+++ src/Controller/RateController.php
@@ -4,9 +4,21 @@
 namespace App\Controller;
 
 
+use App\Entity\Rate;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class RateController extends AbstractController
 {
-    
+    /**
+     * @Route(path="/rate/{id}",name="rate_show")
+     * @return Response
+     */
+    public function show(Rate $rate) :Response{
+
+        return $this->render('rate/show.html.twig',[
+            'rate'=>$rate
+        ]);
+    }
 }
